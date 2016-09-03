24eabdbd2cfb816b11fec701722d178d7820f5b0
ferrantejake <jake.ferrante@hotmail.com>
2016-09-03 19:15:56 -0400

    Revert "initial comit"
    This reverts commit 49ca02e29fe33ea4be2fd41191d725ad456673a3.

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Run `gulp watch` in a terminal and save file");
-        // uncomment below for example error
-        // System.out.println(');
-    }
-}
